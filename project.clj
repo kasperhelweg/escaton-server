00cca720184a9b48110abbda009d035de48f1115
KHJ <kasperhelweg@gmail.com>
2016-03-15 21:37:27 +0100

    add compojure as dep

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.immutant/web "2.1.3"]
-                 [com.taoensso/sente "1.8.1"]]
+                 [com.taoensso/sente "1.8.1"]
+                 [compojure "1.5.0"]]
   :MAIN ^:skip-aot escaton-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
