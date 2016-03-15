a35e31ed233e36e20c4ef1096abef340d7602163
KHJ <kasperhelweg@gmail.com>
2016-03-15 18:04:28 +0100

    initial

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject escaton-server "0.1.0-SNAPSHOT"
+  :description "This is a server prototype of Escaton"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.immutant/web "2.1.3"]
+                 [com.taoensso/sente "1.8.1"]]
+  :MAIN ^:skip-aot escaton-server.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/escaton_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns escaton-server.core-test
+  (:require [clojure.test :refer :all]
+            [escaton-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
